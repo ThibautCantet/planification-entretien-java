71eec532f1792cad0f0f07ade6cc5f4a381578b7
Thibaut Cantet <thibaut.cantet@soat.fr>
2022-12-22 10:08:43 +0100

    extract RendreRecruteurIndisponible use case

--- src/main/java/com/soat/planification_entretien/application_service/recruteur/EntretienCreeListener.java
+++ src/main/java/com/soat/planification_entretien/application_service/recruteur/EntretienCreeListener.java
@@ -1,31 +1,23 @@
 package com.soat.planification_entretien.application_service.recruteur;
 
-import java.util.Optional;
-
 import com.soat.planification_entretien.application_service.Listener;
 import com.soat.planification_entretien.application_service.MessageBus;
 import com.soat.planification_entretien.domain.entretien.EntretienCréé;
-import com.soat.planification_entretien.domain.recruteur.Recruteur;
-import com.soat.planification_entretien.domain.recruteur.RecruteurRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EntretienCreeListener implements Listener<EntretienCréé> {
-    private final RecruteurRepository recruteurRepository;
+    private final RendreRecruteurIndisponible rendreRecruteurIndisponible;
     private final MessageBus messageBus;
 
-    public EntretienCreeListener(RecruteurRepository recruteurRepository, MessageBus messageBus) {
-        this.recruteurRepository = recruteurRepository;
+    public EntretienCreeListener(RendreRecruteurIndisponible rendreRecruteurIndisponible, MessageBus messageBus) {
+        this.rendreRecruteurIndisponible = rendreRecruteurIndisponible;
         this.messageBus = messageBus;
         this.messageBus.subscribe(this);
     }
 
     @Override
     public void onMessage(EntretienCréé entretienCréé) {
-        var maybeRecruteur = recruteurRepository.findById(entretienCréé.recruteurId());
-        maybeRecruteur.ifPresent(recruteur -> {
-            recruteur.rendreIndisponible();
-            recruteurRepository.save(recruteur);
-        });
+        rendreRecruteurIndisponible.execute(entretienCréé.recruteurId());
     }
 }
