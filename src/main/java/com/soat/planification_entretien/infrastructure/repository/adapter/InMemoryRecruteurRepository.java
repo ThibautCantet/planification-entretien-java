93e52e38af7579820f1a3115e4b24d2954c5fbbd
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-10-02 11:23:00 +0200

    implement in memory adapter

---
+++ src/main/java/com/soat/planification_entretien/infrastructure/repository/adapter/InMemoryRecruteurRepository.java
@@ -0,0 +1,32 @@
+package com.soat.planification_entretien.infrastructure.repository.adapter;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import com.soat.planification_entretien.application.use_case.output_port.RecruteurPort;
+import com.soat.planification_entretien.domain.model.Recruteur;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import static java.util.Optional.*;
+
+@Repository
+@Primary
+public class InMemoryRecruteurRepository implements RecruteurPort {
+    private final Map<Integer, Recruteur> map = new HashMap<>();
+
+    @Override
+    public int save(Recruteur recruteur) {
+        int id = map.size() + 1;
+        Recruteur toSave = new Recruteur(id, recruteur.getLanguage(), recruteur.getEmail(), recruteur.getExperienceInYears());
+        map.put(id, toSave);
+        return id;
+    }
+
+    @Override
+    public Optional<Recruteur> findById(int recruteurId) {
+        return Optional.ofNullable(map.getOrDefault(recruteurId, null));
+    }
+
+}
