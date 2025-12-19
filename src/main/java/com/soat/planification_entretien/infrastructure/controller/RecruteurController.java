5922b9fd4aa88ef86714eddcbfa42465cbd16133
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-12-19 13:07:18 +0100

    fix recruteur

--- src/main/java/com/soat/planification_entretien/infrastructure/controller/RecruteurController.java
+++ src/main/java/com/soat/planification_entretien/infrastructure/controller/RecruteurController.java
@@ -1,8 +1,5 @@
 package com.soat.planification_entretien.infrastructure.controller;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import com.soat.planification_entretien.use_case.CreerRecruteur;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +12,6 @@ import static org.springframework.http.ResponseEntity.*;
 @RestController
 @RequestMapping(RecruteurController.PATH)
 public class RecruteurController {
-    private static final String EMAIL_REGEX = "^[\\w-_.+]*[\\w-_.]@([\\w]+\\.)+[\\w]+[\\w]$";
     public static final String PATH = "/api/recruteur";
 
     private final CreerRecruteur creerRecruteur;
@@ -34,11 +30,5 @@ public class RecruteurController {
         }
 
         return created(null).body(savedRecruteur.getId());
-    }
-
-    private static boolean isEmail(String adresse) {
-        final Pattern r = Pattern.compile(EMAIL_REGEX);
-        final Matcher m = r.matcher(adresse);
-        return m.matches();
     }
 }
